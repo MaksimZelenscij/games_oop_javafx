432e365cca57e7b2aec857f87d0a3385e33189d4
MaksimZelenskii <zelmax1988@gmail.com>
2021-12-06 22:29:20 +0400

    Add win check method

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,7 +2,37 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        boolean rsl = true;
-        return rsl;
+        boolean result = false;
+        for (int j = 0; j < board.length; j++) {
+            if ((monoHorizontal(board, j) || monoVertical(board, j))) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static boolean monoHorizontal(int[][] board, int row) {
+        boolean result = true;
+        for (int line = 0; line < board[row].length; line++) {
+            if (board[row][line] != 1) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static boolean monoVertical(int[][] board, int column) {
+        boolean result = true;
+        for (int pillar = 0; pillar < board[column].length; pillar++) {
+            if (board[pillar][column] != 1) {
+                result = false;
+                break;
+            }
+        }
+        return result;
     }
 }
+
+
