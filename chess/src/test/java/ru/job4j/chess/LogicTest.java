5f09d9bb10cc5fc0ce411daf26bafdaf6ac9b5c8
MaksimZelenskii <zelmax1988@gmail.com>
2022-06-02 15:12:23 +0400

    Fixed logic for BlackBishop figure and tests

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -3,29 +3,23 @@ package ru.job4j.chess;
 import org.junit.Test;
 import ru.job4j.chess.firuges.black.BishopBlack;
 
-import static org.junit.Assert.assertThrows;
 import static ru.job4j.chess.firuges.Cell.*;
 
 public class LogicTest {
 
-    @Test
-    public void move() throws FigureNotFoundException, OccupiedCellException {
+    @Test(expected = FigureNotFoundException.class)
+    public void shouldThrowExceptionWhenFigureNotFound()
+            throws OccupiedCellException, FigureNotFoundException {
         Logic logic = new Logic();
-        logic.add(new BishopBlack(C1));
-        logic.move(C1, H6);
+        logic.move(C2, H6);
     }
 
-    @Test
-    public void shouldThrowExceptionWhenFigureNotFound() {
-        Logic logic = new Logic();
-        assertThrows(FigureNotFoundException.class, () -> logic.move(C2, H6));
-    }
-
-    @Test
-    public void shouldThrowExceptionWhenCellOccupied() {
+    @Test(expected = OccupiedCellException.class)
+    public void shouldThrowExceptionWhenCellOccupied()
+            throws OccupiedCellException, FigureNotFoundException {
         Logic logic = new Logic();
         logic.add(new BishopBlack(C1));
         logic.add(new BishopBlack(H6));
-        assertThrows(OccupiedCellException.class, () -> logic.move(C1, H6));
+        logic.move(C1, H6);
     }
 }
